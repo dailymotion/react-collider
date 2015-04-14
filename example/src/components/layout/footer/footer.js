076e2f87ed6d7f1d256a378f40e4319649e983da
romainberger <romain@romainberger.com>
2015-04-14 10:55:16 +0200

    Tweak example

--- example/src/components/layout/footer/footer.js
+++ example/src/components/layout/footer/footer.js
@@ -3,9 +3,19 @@ import React from 'react'
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer className="row">
-                <div className="col-sm-12">
-                    React Collider
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h3>React Collider</h3>
+                        </div>
+                        <div className="col-sm-3">
+                            by <a href="http://dailymotion.com">Dailymotion</a>
+                        </div>
+                        <div className="col-sm-3">
+                            <a className="btn btn-default" href="https://github.com/youpinadi/react-collider">View on GitHub</a>
+                        </div>
+                    </div>
                 </div>
             </footer>
         )
--- example/src/components/player/player.js
+++ example/src/components/player/player.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default class Player extends React.Component {
     render() {
         return (
-            <iframe className="player"
-                src={'http://dailymotion.com/embed/video/' + this.props.id}
-                width="960" height="540" />
+            <div className="player">
+                <iframe
+                    src={'http://dailymotion.com/embed/video/' + this.props.id}
+                    width="960" height="540" />
+            </div>
         )
     }
 }

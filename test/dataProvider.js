2bddb86b4f4f57a494e03e4a66cacc0e8af7a5dc
romainberger <romain@romainberger.com>
2015-05-13 16:01:59 +0200

    Fix json encoding

--- test/dataProvider.js
+++ test/dataProvider.js
@@ -22,53 +22,53 @@ describe('Data Provider', function() {
 
     it('should fetch data from a url', function(done) {
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title').then(function(data) {
-            expect(data).to.be.a('string')
-            expect(JSON.parse(data).title).to.equal(videoData.title)
+            expect(data).to.be.an('object')
+            expect(data.title).to.equal(videoData.title)
             done()
         })
     })
 
     it('should fetch data from local variable', function(done) {
-        global.initialData = {Video: JSON.stringify(localData)}
+        global.initialData = {Video: localData}
 
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title').then(function(data) {
-            expect(data).to.be.a('string')
-            expect(JSON.parse(data).title).to.equal(localData.title)
+            expect(data).to.be.an('object')
+            expect(data.title).to.equal(localData.title)
             done()
         })
     })
 
     it('should fetch data from local variable without removing it', function(done) {
-        global.initialData = {Video: JSON.stringify(localData)}
+        global.initialData = {Video: localData}
 
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title', {once: false}).then(function(data) {
-            expect(JSON.parse(data).title).to.equal(localData.title)
+            expect(data.title).to.equal(localData.title)
 
             provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title').then(function(data) {
-                expect(JSON.parse(data).title).to.equal(localData.title)
+                expect(data.title).to.equal(localData.title)
                 done()
             })
         })
     })
 
     it('should fetch data from local variable then remove it fetch from url', function(done) {
-        global.initialData = {Video: JSON.stringify(localData)}
+        global.initialData = {Video: localData}
 
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title', {once: true}).then(function(data) {
-            expect(JSON.parse(data).title).to.equal(localData.title)
+            expect(data.title).to.equal(localData.title)
 
             provider('Video', 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title').then(function(data) {
-                expect(JSON.parse(data).title).to.equal(videoData.title)
+                expect(data.title).to.equal(videoData.title)
                 done()
             })
         })
     })
 
     it('should force fetching from the url even the local data exists', function(done) {
-        global.initialData = {Video: JSON.stringify(localData)}
+        global.initialData = {Video: localData}
 
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title', {forceFetch: true}).then(function(data) {
-            expect(JSON.parse(data).title).to.equal(videoData.title)
+            expect(data.title).to.equal(videoData.title)
             done()
         })
     })
@@ -77,10 +77,10 @@ describe('Data Provider', function() {
         global.initialData = {}
 
         provider(Video, 'https://api.dailymotion.com/video/' + videoId + '?fields=id,title', {set: true}).then(function(data) {
-            expect(JSON.parse(data).title).to.equal(videoData.title)
+            expect(data.title).to.equal(videoData.title)
 
             provider(Video, 'https://api.dailymotion.com/video/' + otherId + '?fields=id,title').then(function(data) {
-                expect(JSON.parse(data).title).to.equal(videoData.title)
+                expect(data.title).to.equal(videoData.title)
                 done()
             })
         })
